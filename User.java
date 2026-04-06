ef311e3af1b60adbb697ce1f3233f64596586c57
reddycharankumar513-dotcom <charankumarreddy149@gmail.com>
2026-04-06 08:10:00 +0530

    Create User class with properties and constructor

---
+++ User.java
@@ -0,0 +1,21 @@
+public class User {
+    int    userId;
+    String name;
+    String email;
+    String phone;
+    String password;
+
+    public User(int userId, String name, String email, String phone, String password) {
+        this.userId   = userId;
+        this.name     = name;
+        this.email    = email;
+        this.phone    = phone;
+        this.password = password;
+    }
+
+    public int    getUserId()  { return userId;   }
+    public String getName()    { return name;     }
+    public String getEmail()   { return email;    }
+    public String getPhone()   { return phone;    }
+    public String getPassword(){ return password; }
+}
